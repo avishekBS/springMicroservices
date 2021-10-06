15b47b122a4a642b6d12a7929de5e821ef4bc955
avishekBS <a.b.sribasbanikam@gmail.com>
2021-10-06 21:56:39 +0530

    add logging configuration

--- employee-api/src/main/java/com/codeolate/employee/api/controllers/EmployeeResource.java
+++ employee-api/src/main/java/com/codeolate/employee/api/controllers/EmployeeResource.java
@@ -2,6 +2,8 @@ package com.codeolate.employee.api.controllers;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,6 +28,8 @@ import com.codeolate.employee.api.services.UpdateEmployeeService;
 @RequestMapping(path="/employee")
 public class EmployeeResource {
 	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	private AddEmployeeService addEmployeeService;
 	
@@ -49,34 +53,41 @@ public class EmployeeResource {
 	
 	@PostMapping()
 	private String addEmployee(@Valid @RequestBody Employee employee) {
+		logger.trace("Add Employee resource");
 		return addEmployeeService.addEmployee(employee);
 	}
 	
 	@GetMapping()
 	public EmployeeListWrapper getAllEmployee() {
+		logger.trace("get all Employee resource");
 		return getAllEmployeeService.getAllEmployee();
 	}
 	
 	@GetMapping("/{id}")
 	public Employee getEmployeeById(@PathVariable long id) {
+		logger.trace("get all Employee resource by Employee id");
 		return getEmployeeByIdService.getEmployeeById(id);
 	}
 	@GetMapping("/name/{name}")
 	public EmployeeListWrapper getEmployeeByName(@PathVariable String name) {
+		logger.trace("get all Employee resource by Employee Name");
 		return getEmployeeByNameService.getEmployeeByName(name);
 	}
 	@GetMapping("/year/{year}")
 	public EmployeeListWrapper getEmployeeByJoiningYear(@PathVariable int year) {
+		logger.trace("get all Employee resource with joining year "+ year);
 		return getEmployeeByJoiningYearService.getEmployeeByJoiningYear(year);
 	}
 	
 	@PutMapping("/{id}")
-	public Employee getAllEmployee(@PathVariable long id, @Valid @RequestBody Employee employee) {
+	public Employee updateEmployee(@PathVariable long id, @Valid @RequestBody Employee employee) {
+		logger.trace("update employee id "+ id);
 		return updateEmployeeService.updateEmployee(id, employee);
 	}
 	
 	@DeleteMapping("/{id}")
-	public String getAllEmployee(@PathVariable long id) {
+	public String deleteEmployeeDetails(@PathVariable long id) {
+		logger.trace("delete employee id "+ id);
 		return deleteEmployeeService.deleteEmployeeDetails(id);
 	}
 
