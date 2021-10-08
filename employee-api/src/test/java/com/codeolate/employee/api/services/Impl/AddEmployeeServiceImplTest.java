9f4f7b309c3c13fa949a1c52549c12fba49a7ec6
avishekBS <a.b.sribasbanikam@gmail.com>
2021-10-08 17:42:25 +0530

    Junit 5

---
+++ employee-api/src/test/java/com/codeolate/employee/api/services/Impl/AddEmployeeServiceImplTest.java
@@ -0,0 +1,22 @@
+package com.codeolate.employee.api.services.Impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddEmployeeServiceImplTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
